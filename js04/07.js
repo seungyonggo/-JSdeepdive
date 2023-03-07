3e04767c7bc05b7ddaa07f139e93a560c39c94ca
seingyonggo <tmddyd432@gmail>
2023-03-07 18:21:27 +0900

    '23.03.06'

--- js04/07.js
+++ js04/07.js
@@ -172,3 +172,158 @@ NaN === NaN;
 Number.isNaN(NaN);
 Number.isNaN(10);
 Number.isNaN(1 + undefined);
+
+// 예제 07-17
+0 === -0;
+0 == -0;
+
+// 예제 07-18
+-0 === +0;
+Object.is(-0, +0);
+
+NaN === NaN;
+Object.is(NaN, NaN);
+
+// 예제 07-19
+
+5 != 8;
+5 != 5;
+5 != "5";
+
+5 !== 8;
+5 !== 5;
+5 !== "5";
+
+// 예제 07-20
+5 > 0;
+5 > 5;
+5 >= 5;
+5 <= 5;
+
+// 예제 07-21
+var f = 2;
+var result = f % 2 ? "홀수" : "짝수";
+
+console.log(result);
+
+// 예제 07-22
+var g = 2,
+  result;
+
+if (g % 2) result = "홀수";
+else result = "짝수";
+
+console.log(result);
+
+// 예제 07-23
+var q = 10;
+
+var result = if( q % 2) {result = '홀수'} else {result = '짝수'}
+
+// 예제 07-24
+
+var w = 10;
+
+var result = w % 2 ? '홀수' : '짝수';
+console.log(result);
+
+// 예제 07-25
+true || true;
+true || false;
+false || true;
+false || false;
+
+true && true;
+true && false;
+false && true;
+false && false;
+
+!true;
+!false;
+
+// 예제 07-26
+!0;
+!'Hello';
+
+// 예제 07-27
+'Cat' && 'Dog';
+
+// 예제 07-28
+!(x || y) === (!x && !y)
+!(x && y) === (!x || !y)
+
+// 예제 07-29
+var x, y, z;
+
+x = 1, y = 2, z = 3;
+
+// 예제 07-30
+
+10 * 2 + 3;
+
+10 * (2 + 3);
+
+// 예제 07-31
+typeof ''
+typeof 1
+typeof NaN
+typeof true
+typeof undefined
+typeof Symbol()
+typeof null
+typeof []
+typeof {}
+typeof new Date()
+typeof /test/gi
+typeof function() {}
+
+// 예제 07-32
+
+var foo = null;
+
+typeof foo === null;
+foo === null;
+
+// 예제 07-33
+typeof undefined;
+
+// 예제 07-34
+2 ** 2;
+2 ** 2.5;
+2 ** 0;
+2 ** -2;
+
+// 예제 07-35
+Math.pow(2, 2);
+Math.pow(2, 2.5);
+Math.pow(2, 0);
+Math.pow(2, -2);
+
+// 예제 07-36
+2 ** (3 ** 2);
+Math.pow(2, Math.pow(3, 2));
+
+// 예제 07-37
+-5 ** 2;
+(-5) ** 2;
+
+// 예제 07-38
+var num = 5;
+num **= 2;
+
+// 예제 07-39
+2 * 5 ** 2;
+
+// 예제 07-40
+var x;
+x = 1;
+console.log(x);
+x++;
+console.log(x);
+
+var o = {a : 1};
+delete o.a;
+console.log(o);
+
+// 예제 07-41
+console.log(10 * (2 + 3));
